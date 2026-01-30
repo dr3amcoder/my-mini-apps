a86b07c211a747671c7d69b0f75fb41599607243
Janna.Limpin <janna.limpin@comparethemarket.com>
2026-01-30 16:09:58 +0000

    Pokemon search improvement

--- src/components/PokemonSearch.jsx
+++ src/components/PokemonSearch.jsx
@@ -2,40 +2,63 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const PokemonSearch = () => {
-  const [pokemonName, setPokemonName] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [pokemonName, setPokemonName] = useState("Pikachu");
   const [pokemonSprite, setPokemonSprite] = useState(
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif",
   );
-  const [pokemonID, setPokemonID] = useState(1);
-  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonID, setPokemonID] = useState("25");
+  const [pokemonType, setPokemonType] = useState("Electric");
   const [pokemonCries, setPokemonCries] = useState(
-    "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/1.ogg",
+    "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/25.ogg",
   );
 
-  useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/1").then((response) => {
-      console.log(response.data);
-      setPokemonName(response.data.name);
-      setPokemonSprite(
-        response.data.sprites.versions["generation-v"]["black-white"].animated
-          .front_default,
-      );
-      setPokemonID(response.data.id);
-      setPokemonType(response.data.types[0].type.name);
-      setPokemonCries(response.data.cries.latest);
-    });
-  }, []);
+  const handleSearch = () => {
+    // useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${searchInput}`)
+      .then((response) => {
+        console.log(response.data);
+        setPokemonName(response.data.name);
+        setPokemonSprite(
+          response.data.sprites.versions["generation-v"]["black-white"].animated
+            .front_default,
+        );
+        setPokemonID(response.data.id);
+        setPokemonType(response.data.types[0].type.name);
+        setPokemonCries(response.data.cries.latest);
+      });
+    // }, [searchInput]);
+  };
 
   return (
     <div className="container">
       <h2 className="title-md">Pokémon Search</h2>
-      <p>{pokemonName.toUpperCase()}</p>
-      <img src={pokemonSprite} alt={pokemonName} />
-      <p>Pokemon ID: {pokemonID}</p>
-      <p>Pokemon Type: {pokemonType.toUpperCase()}</p>
-      <audio controls>
-        <source src={pokemonCries} type="audio/ogg" />
-      </audio>
+      <input
+        className="input"
+        type="text"
+        placeholder="Enter a pokemon name.."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <br />
+      <br />
+      <button className="btn" onClick={handleSearch}>
+        Search
+      </button>
+      <br />
+      <br />
+      <div className="info-box">
+        <p>{pokemonName.toUpperCase()}</p>
+        <img className="pokemon-sprite" src={pokemonSprite} alt={pokemonName} />
+        <p>Pokemon ID: {pokemonID}</p>
+        <p>Pokemon Type: {pokemonType.toUpperCase()}</p>
+        {pokemonCries && (
+          <audio controls key={pokemonCries}>
+            <source src={pokemonCries} type="audio/ogg" />
+          </audio>
+        )}
+      </div>
     </div>
   );
 };
